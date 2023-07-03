78f4b2b5504c0a8afce36e976646022afd395f89
mhuertascompany <marc.huertas@obspm.fr>
2023-07-03 17:42:19 +0200

    shalo debug

--- data/subhalos.py
+++ data/subhalos.py
@@ -8,14 +8,14 @@ fields = ['SubhaloFlag','SubhaloMass','SubhaloHalfmassRad']
 cols=[]
 for i in range(72,91,1):
     subhalos = il.groupcat.loadSubhalos(basePath,i,fields=fields)
-    col1=fits.Column(name='SubhaloMass', format='D', array=subhalos['SubhaloMass'] * 1e10 / 0.704)
+    col1=fits.Column(name='SubhaloMass', format='D', array=subhalos['SubhaloMass'][0] * 1e10 / 0.704)
     print(subhalos['SubhaloMass'].shape)
     cols.append(col1)
-    col2=fits.Column(name='SHID', format='D', array=subhalos['count'])
+    col2=fits.Column(name='SHID', format='D', array=subhalos['count'][0])
     cols.append(col2)
-    col3=fits.Column(name='SubhaloHalfMassRad', format='D', array=subhalos['SubhaloHalfmassRad'])
+    col3=fits.Column(name='SubhaloHalfMassRad', format='D', array=subhalos['SubhaloHalfmassRad'][0])
     cols.append(col3)
-    col4=fits.Column(name='SubhaloFlag', format='D', array=subhalos['SubhaloFlag'])
+    col4=fits.Column(name='SubhaloFlag', format='D', array=subhalos['SubhaloFlag'][0])
     cols.append(col4)
     
     cols_to_write=fits.ColDefs(cols)
