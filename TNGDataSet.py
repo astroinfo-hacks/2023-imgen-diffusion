50598495481a80a9e87a16844b8e1c78a021fe27
Wassim KABALAN <wassimkabalan@gmail.com>
2023-07-05 01:16:56 +0200

    Finished DataSet generator

--- TNGDataSet.py
+++ TNGDataSet.py
@@ -14,6 +14,16 @@ from pathlib import Path
 
 
 def Logger(str,verbosity=0):
+    """
+
+      Simple Logger
+      By default logs .. increase verbosity in case of well .. more verbose logs
+
+      use 
+      os.environ["DIFF_TRACE"]="1"
+      in your notebook to change verbosity
+
+    """
     if int(os.environ.get("DIFF_TRACE",0)) > verbosity:
         print(str)
 
@@ -40,7 +50,7 @@ class SubsplitDictionaries:
     def CreateKey(EXTNAME, ORIGIN, SIMTAG, SNAPNUM, SUBHALO):
         return "{0}_{1}_{2}_{3}_{4}".format(EXTNAME, ORIGIN, SIMTAG, SNAPNUM, SUBHALO)
     
-    # Sanity check (maybe not needed)
+    # Sanity check (This is not needed .. TFDS does it already .. the key is sent back with the yield
     def AlreadyExisting(self,key_with_camera,Create=False):
         res =  key_with_camera not in self.full_list
         if Create:
@@ -48,23 +58,23 @@ class SubsplitDictionaries:
         return res
 
     def FindOrCreate(self,key, CAMERA,subsplit):
-        key_with_camera = "{0}_{1}".format(key,CAMERA)
+        #key_with_camera = "{0}_{1}".format(key,CAMERA)
         #assert (self.AlreadyExisting(key_with_camera,True))
         if subsplit == tfds.Split.TRAIN:
             if key not in self.valid_dict and key not in self.test_dict:
-                Logger("Key {0} with camera code {1} will be assigned to training".format(key,CAMERA),2)
+                Logger("Key {0} with camera code {1} will be assigned to training".format(key,CAMERA),3)
                 if key not in self.train_dict:
                     self.train_dict.append(key)
                 return False
         elif subsplit == tfds.Split.VALIDATION:
             if key not in self.train_dict and key not in self.test_dict:
-                Logger("Key {0} with camera code {1} will be assigned to validation".format(key,CAMERA),2)
+                Logger("Key {0} with camera code {1} will be assigned to validation".format(key,CAMERA),3)
                 if key not in self.valid_dict:
                     self.valid_dict.append(key)
                 return False
         elif subsplit == tfds.Split.TEST:
             if key not in self.train_dict and key not in self.valid_dict:
-                Logger("Key {0} with camera code {1} will be assigned to testing".format(key,CAMERA),2)
+                Logger("Key {0} with camera code {1} will be assigned to testing".format(key,CAMERA),3)
                 if key not in self.test_dict:
                     self.test_dict.append(key)
                 return False
@@ -81,13 +91,21 @@ class TNGDataSet(tfds.core.GeneratorBasedBuilder):
     RELEASE_NOTES = {'1.0.0': 'Initial release.',}
     MANUAL_DOWNLOAD_INSTRUCTIONS = "Nothing to download. Dataset was generated at first call."
 
-    def __init__(self,train_percent=0.8, val_percent=0.1, test_percent=0.1,**kwargs):
+    def __init__(
+            self,train_percent=0.8, 
+            val_percent=0.1, 
+            test_percent=0.1,
+            Scaler_fcn=ScaleImage ,
+            generation_verbosity=1000,
+            band_filters=['CFHT_MEGACAM.U', 'SUBARU_HSC.G' ,'SUBARU_HSC.R' ,'CFHT_MEGACAM.R' ,'SUBARU_HSC.I', 'SUBARU_HSC.Z' ,'SUBARU_HSC.Y'],
+            **kwargs):
         super(TNGDataSet,self).__init__(**kwargs)
         self.internal_dict = SubsplitDictionaries()
         self.isPopulated = False
         self.list_of_fits = []
-        # make sure that there is no funny business (all sets add to 1)
-        assert (train_percent + val_percent + test_percent) == 1
+        # Notify user if he is using wierd percentages
+        if (train_percent + val_percent + test_percent) != 1:
+            Logger("TNGDataSet WARNING : Only {0}% of the data will be assigned")
         # register percenteges
         self.train_percent = train_percent
         self.val_percent = val_percent
@@ -96,7 +114,13 @@ class TNGDataSet(tfds.core.GeneratorBasedBuilder):
         self.nb_of_train = 0
         self.nb_of_val = 0
         self.nb_of_test = 0
-
+        # Define the scaler function
+        self.Scaler_fcn = Scaler_fcn
+        # Define selected filters
+        self.band_filters = band_filters
+        # helper
+        self.hit_count = {}
+        self.generation_verbosity = generation_verbosity
     
     def PopulateFileList(self,fit_path):
         if self.isPopulated:
@@ -106,12 +130,17 @@ class TNGDataSet(tfds.core.GeneratorBasedBuilder):
 
         for root, dirs, files in os.walk(fit_path, topdown=False):
             for name in files:
-                self.list_of_fits.append(os.path.join(root, name))
+                file_path = os.path.join(root, name)
+                if os.path.splitext(file_path)[1] != ".fits":
+                    continue
+                self.list_of_fits.append(file_path)
         # Make sure that some data exists
         assert len(self.list_of_fits) > 0
         self.isPopulated = True
-        # 7 should be a parameter
-        self.nb_of_entries = len(self.list_of_fits) * 7 
+        # nb_of_entries  is number of files times number of selected filters
+        self.nb_of_entries = len(self.list_of_fits) * len(self.band_filters)
+        # Called only once .. so not verbose
+        Logger("File list is populated .. there is {0} files with {1} entries".format(len(self.list_of_fits),self.nb_of_entries))
     
     def EnoughSamples(self,split_type):
         if split_type == tfds.Split.TRAIN:
@@ -124,14 +153,20 @@ class TNGDataSet(tfds.core.GeneratorBasedBuilder):
     def IncrementSamples(self,split_type):
         if split_type == tfds.Split.TRAIN:
             self.nb_of_train += 1
+            if self.nb_of_train % self.generation_verbosity == 0:
+                Logger("Number of Training samples is {0} ".format(self.nb_of_train),1)
+                Logger("Remaining to add {0}".format(int(self.nb_of_entries * self.train_percent)),1)
         elif split_type == tfds.Split.VALIDATION:
             self.nb_of_val += 1
+            if self.nb_of_val % self.generation_verbosity == 0:
+                Logger("Number of Validation samples is {0}".format(self.nb_of_val),1)
+                Logger("Remaining to add {0}".format(int(self.nb_of_entries * self.val_percent)),1)
         elif split_type == tfds.Split.TEST:
             self.nb_of_test += 1
+            if self.nb_of_test % self.generation_verbosity == 0:
+                Logger("Number of Test samples is {0}".format(self.nb_of_test),1)
+                Logger("Remaining to add {0}".format(int(self.nb_of_entries * self.test_percent)),1)
 
-        if self.nb_of_train%10 == 0:
-            print("train is ",self.nb_of_train)
-            print("train should be ",(self.nb_of_entries * self.train_percent))
 
     def _info(self) -> tfds.core.DatasetInfo:
         """Returns the dataset metadata."""
@@ -170,19 +205,24 @@ class TNGDataSet(tfds.core.GeneratorBasedBuilder):
 
         # Only populated the first time
         self.PopulateFileList(fit_path)
-        hit_count = {}
 
         while len(self.list_of_fits) > 0 and not self.EnoughSamples(split_type):
             # Select randomly a file
             fit_file = choice(self.list_of_fits)
 
-            Logger("File : {0} is being treated".format(fit_file),1)
+            # very verbose
+            Logger("File : {0} is being treated".format(fit_file),2)
 
             fitm = fits.open(fit_file)
             for fit_elem in fitm:
                 example = {}
 
                 example["EXTNAME"] = fit_elem.header["EXTNAME"]
+                # Filter the extension :)
+                if example["EXTNAME"] not in self.band_filters:
+                    continue
+
+                example["FILTER"] = fit_elem.header["FILTER"]
                 example["ORIGIN"] = fit_elem.header["ORIGIN"]
                 example["SIMTAG"] = fit_elem.header["SIMTAG"]
                 example["SNAPNUM"] = fit_elem.header["SNAPNUM"]
@@ -191,28 +231,29 @@ class TNGDataSet(tfds.core.GeneratorBasedBuilder):
 
                 key = SubsplitDictionaries.CreateKey(example["EXTNAME"],example["ORIGIN"],example["SIMTAG"],
                                                     example["SNAPNUM"],example["SUBHALO"])
+                # Extremely verbose
                 if self.internal_dict.FindOrCreate(key,example["CAMERA"],split_type):
                     continue
                 
                 example["REDSHIFT"] = fit_elem.header["REDSHIFT"]
-                example["FILTER"] = fit_elem.header["FILTER"]
                 example["FOVSIZE"] = fit_elem.header["FOVSIZE"]
                 example["BUNIT"] = fit_elem.header["BUNIT"]
                 example["NAXIS1"] = fit_elem.header["NAXIS1"]
                 example["NAXIS2"] = fit_elem.header["NAXIS2"]
-                example["img"] = ScaleImage(fit_elem.data)
+                example["img"] = self.Scaler_fcn(fit_elem.data)
 
                 # Register a hit for this file
-                if hit_count.get(fit_file,0) != 0:
-                    hit_count[fit_file] += 1
+                if self.hit_count.get(fit_file,0) != 0:
+                    self.hit_count[fit_file] += 1
                 else:
-                    hit_count[fit_file] = 1
+                    self.hit_count[fit_file] = 1
                 self.IncrementSamples(split_type)
-                Logger("Hit count for {0} is {1}".format(fit_file,hit_count[fit_file]),2)
+                Logger("Hit count for {0} is {1}".format(fit_file,self.hit_count[fit_file]),2)
 
-                yield "I" , example
+                key_with_camera = "{0}_{1}".format(key,example["CAMERA"])
+                yield key_with_camera , example
 
             # Done with this file
-            # 7 sould be a parameter probably
-            if hit_count.get(fit_file,0) == 7:
+            # If we took all data with selected filters this file is done remove it
+            if self.hit_count.get(fit_file,0) == len(self.band_filters):
                 self.list_of_fits.remove(fit_file)
